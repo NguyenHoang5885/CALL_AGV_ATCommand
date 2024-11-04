59d4fd87cb764f8fa3c4784ed45b288d6a3754cb
Nguyen_Hoang <157201644+NguyenHoang5885@users.noreply.github.com>
2024-11-04 16:54:57 +0700

    Server_Client.py

--- src/Server_Client.py
+++ src/Server_Client.py
@@ -1,17 +1,16 @@
 #code flask
 from flask import Flask, request, jsonify
 
-# Khởi tạo ứng dụng Flask
+# Khởi tạo Flask
 app = Flask(__name__)
 
-# Định nghĩa route với phương thức POST
 @app.route('/items', methods=['POST'])
 def create_item():
     try:
-    # Lấy dữ liệu JSON từ request body
+    # Lấy dữ liệu JSON 
         data = request.get_json()
         print(data)
-        # Xử lý dữ liệu (ví dụ lưu vào database hoặc thực hiện tính toán)
+        # Xử lý dữ liệu 
         item_name = data.get('name')
         item_Value = data.get('Value')
 
@@ -23,7 +22,7 @@ def create_item():
                 'Value': item_Value
             }
         }
-        return jsonify(response), 200  # Trả về HTTP status 201 (Created)
+        return jsonify(response), 200  
     except Exception as e :
         print(e)
         response = {
@@ -35,7 +34,7 @@ def create_item():
 def hello():
     try:
         response = {"a" : "123"}
-        return jsonify(response), 200  # Trả về HTTP status 201 (Created)
+        return jsonify(response), 200 
     except Exception as e :
         print(e)
         response = {
@@ -43,5 +42,4 @@ def hello():
         }
         return jsonify(response), 400
 if __name__ == '__main__':
-    # Chạy ứng dụng Flask trên cổng 5000
     app.run(debug=True, host="0.0.0.0", port=5000)
